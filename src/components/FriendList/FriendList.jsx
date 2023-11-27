378ca1ce28359b8eaf7d7b9ee1106e4725d94183
Kostya Semenchuk <75216256+modie22@users.noreply.github.com>
2023-11-27 20:56:57 +0200

    add-tasks

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types'
+import css from './FriendList.module.css'
+
+export const FriendList = ({ children }) => {
+  return (
+    <ul className={css.friends__list}>{children}</ul>
+  )
+}
+FriendList.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+import css from './FriendList.module.css'
+
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={css.friends__item}>
+      <span
+        style={{ backgroundColor: isOnline ? 'green' : 'red' }}
+        className={css.friends__status}
+      ></span>
+      <img className={avatar} src={avatar} alt={name} width="48" />
+      <p className={css.friends__name}>{name}</p>
+    </li>
+  )
+}
+
+FriendListItem.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool,
+  };
